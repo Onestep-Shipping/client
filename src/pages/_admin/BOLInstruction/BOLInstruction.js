7b759e051112197f2ec102a352ecdd08eceeb608
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-04-27 18:49:06 -0700

    Lay down basic structure for Admin

--- src/pages/_admin/BOLInstruction/BOLInstruction.js
+++ src/pages/_admin/BOLInstruction/BOLInstruction.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './BOLInstruction.css';
 
 import Header from '../../../components/Header/Header.js';
+import DATA from '../../../data/ScheduleDetailsData.js';
 
 const BOLInstruction = () => {
   return (
     <div className="homepage-container">
       <Header />
-      <div className="body-container2">
-        BOL Instruction
+      <div className="bol-instruction-container">
+          <ul className="bol-instruction-list"> 
+            {DATA.map((schedule, ind) => (
+                <li className="bol-instruction-item" key={ind}>HELLO</li>
+              )
+            )}
+          </ul>
+        <div className="bol-instruction-detail"> </div>
       </div>
     </div>
   );
