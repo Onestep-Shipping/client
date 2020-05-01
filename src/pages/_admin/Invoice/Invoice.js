11a10d3090c6ed4184ff6048f94ba15a39acd634
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-01 15:24:13 -0700

    Complete admin form for Invoice

--- src/pages/_admin/Invoice/Invoice.js
+++ src/pages/_admin/Invoice/Invoice.js
@@ -1,14 +1,151 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Invoice.css';
 
 import Header from '../../../components/Header/Header.js';
 
+import BookingDisplay from '../../../components/BookingDisplay/BookingDisplay.js';
+import UserList from '../../../components/UserList/UserList.js';
+import INVOICE from '../../../data/InvoiceData.js';
+import { InfoRow } from '../Helpers.js';
+import pdfGenerator from './pdfGenerator.js';
+
+const comma = x => {
+  return Number(x).toLocaleString()
+}
+
 const Invoice = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [fees, setFees] = useState(
+    Array(INVOICE[currentIndex].booking.container.length + 2).fill(0)
+  );
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    createInfoObject(e);
+    // const pdf = pdfGenerator(createInfoObject(e));
+    // var data = new FormData();
+    // data.append('data', pdf);
+
+    // TODO: Fetch formData to server
+  }
+
+  const onFeeChange = (e, i) => {
+    let newFees = [...fees]; 
+    let newVal = e.target.value;
+    if (i < fees.length - 2) {
+      newVal *=  INVOICE[currentIndex].booking.container[i].quantity;
+    }
+    newFees[i] = newVal;
+    setFees(newFees);
+  }
+
+  const createInfoObject = e => {
+    const { price, total, subTotal } = e.target;
+
+    price.forEach(node => {
+      console.log("Price: " + node.value);
+    })
+  }
+
   return (
     <div className="homepage-container">
       <Header />
-      <div className="body-container2">
-        Invoice
+      <div className="bol-instruction-container">
+        <UserList 
+          setInd={setCurrentIndex}
+          opt={INVOICE} type="bol" />
+        <div className="bol-instruction-detail"> 
+          <div className="booking-id-container">
+            <h1>Booking #{INVOICE[currentIndex].id}</h1>
+          </div>
+          <div className="customer-info-container">
+            <text>Company: {INVOICE[currentIndex].company.name}</text>
+            <text>Email: {INVOICE[currentIndex].email}</text>
+          </div>
+
+          <div className="form-container">
+            <h2>Schedule</h2>
+            <BookingDisplay id={'' + currentIndex} fields={8}/>
+          </div>
+
+          <div className="form-container">
+            <h2>Information</h2>
+            <div className="booking-details-container">
+              <InfoRow label="Booking Number" value={INVOICE[currentIndex].id} />
+              <InfoRow label="Commodity" value={INVOICE[currentIndex].booking.commodity} />
+              <InfoRow label="Order/PO Number" value={INVOICE[currentIndex].bol.orderNo} />
+            </div>
+          </div>
+
+          <form className="invoice-form-container" onSubmit={handleSubmit}>
+            <h2>Invoice</h2>
+            <div className="invoice-row">
+                <text className="info-label-special">Description</text>
+                <text className="info-label-special">Quantity</text>
+                <text className="info-label-special">Container</text>
+                <div className="usd-input-container">
+                  <text className="info-label-special">Price (USD)</text>
+                  <text className="info-label-special">Total (USD)</text>
+                </div>
+            </div>
+            {INVOICE[currentIndex].booking.container.map((row, ind) => (
+              <div className="invoice-row" key={ind}>
+                <text>Ocean Freight</text>
+                <text>{row.quantity}</text>
+                <text>{row.containerType}</text>
+                <div className="usd-input-container">
+                <input 
+                  type="text" name="price" className="usd-input" 
+                  onChange={e => onFeeChange(e, ind)}
+                />
+                <input 
+                  type="text" name="total" className="usd-input" 
+                  disabled={true} value={comma(fees[ind])}
+                />
+              </div>
+              </div>
+            ))}
+            <div className="invoice-row">
+              <text>Document Fee</text>
+              <div className="usd-input-container">
+                <input 
+                  type="text" name="price" className="usd-input" 
+                  onChange={e => onFeeChange(e, fees.length - 2)}
+                />
+                <input 
+                  type="text" name="total" className="usd-input" 
+                  disabled={true} value={comma(fees[fees.length - 2])}
+                />
+              </div>
+            </div>
+            <div className="invoice-row">
+              <text>Administration Fee</text>
+              <div className="usd-input-container">
+                <input 
+                  type="text" name="price" className="usd-input" 
+                  onChange={e => onFeeChange(e, fees.length - 1)}
+                />
+                <input 
+                  type="text" name="total" className="usd-input" 
+                  disabled={true} value={comma(fees[fees.length - 1])}
+                />
+              </div>
+            </div>
+            <div className="invoice-row-right">
+              <text>Subtotal</text>
+              <input 
+                type="text" name="subTotal" className="total-input" 
+                disabled={true} value={comma(fees.reduce((a, b) => a + b, 0))}
+              />
+            </div>
+            <div className="bol-button-form">
+              <input id="left-button" type="submit" className="result-button" value="Generate PDF" />
+              <button  className="result-button">
+                Send to Customer
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/_admin/Invoice/pdfGenerator.js
@@ -0,0 +1,110 @@
+import jsPDF from 'jspdf';
+import moment from 'moment';
+
+const doc = new jsPDF("p", "pt", 'letter');
+
+const centeredText = (text, y) => {
+    let textWidth = doc.getStringUnitWidth(text) * doc.internal.getFontSize() / doc.internal.scaleFactor;
+    let textOffset = (doc.internal.pageSize.width - textWidth) / 2;
+    doc.text(textOffset, y, text);
+}
+
+const pdfGenerator = info => {
+  doc.setProperties({
+    title: 'Booking Confirmation #' + info.bookingNo,
+    subject: 'Info about PDF',
+    author: 'Scarlet Nguyen',
+    keywords: 'generated, javascript, web 2.0, ajax',
+    creator: 'OneStep Shipping'
+  });
+
+  doc.setFont('helvatica');
+  doc.setFontSize(16);
+  doc.setFontType('bolditalic');
+  doc.text(430, 60, "OneStep Shipping");
+
+  doc.setFont('courier');
+  doc.setFontSize(20);
+  doc.setFontType('bold');
+  centeredText('Booking Confirmation', 100);
+
+  doc.setFontSize(12);
+  doc.setFontType('normal');
+  doc.text(50, 140, "TO: " + info.company.name);
+  doc.text(50, 158, "    " + info.company.address.line1);
+  doc.text(50, 173, "    " + info.company.address.line2);
+  doc.text(50, 188, "    " + info.company.address.line3);
+
+  doc.setFontSize(10);
+  doc.text(310, 130, "Our Reference No.:                1234567");
+  doc.text(310, 145, "Contact          :              Rose Phan");
+  doc.text(310, 160, "Tel              :      +1 (604) 500-4934");
+  doc.text(310, 175, "Email            :   rosephan99@gmail.com");
+  doc.text(310, 190, "Issue Date       :             " + moment().format("L"));
+
+  doc.line(50, 200, 560, 200);
+
+  doc.setFontSize(11);
+  doc.text(80, 230, "From: " + info.schedule.from);
+  doc.text(350, 230, "To: " + info.schedule.to);
+
+  doc.text(80, 250, "ETD: " + info.schedule.fromDate);
+  doc.text(350, 250, "ETA: " + info.schedule.toDate);
+
+  doc.text(80, 290, "Commodity: " + info.booking.commodity);
+
+  doc.text(350, 290, "Q'ty");
+  doc.text(420, 290, "Container Type");
+
+  info.booking.container.map((row, i) => {
+    doc.text(350, 307 + (i * 17), "" + row.quantity);
+    doc.text(420, 307 + (i * 17), row.containerType);
+  })
+
+  doc.setFontSize(12);
+  doc.setFontType('bold');
+  doc.text(80, 330, "Booking No.: " + info.bookingNo);
+
+  doc.setFontSize(11);
+  doc.setFontType('normal');
+  doc.text(80, 360, "Carrier Name: Hapag-Lloyd");
+
+  doc.text(80, 400, "Terminal Cut-off: " + info.terminalDate);
+  doc.text(80, 420, "Document Cut-off: " + info.docDate);
+  doc.text(80, 440, "VGM Cut-off     : " + info.vgmDate);
+
+  doc.setFontSize(12);
+  doc.setFontType('bold');
+  doc.text(80, 483, "Empty Pickup Location");
+  doc.text(350, 483, "Return Location");
+
+  doc.setFontType('normal');
+  doc.setFontSize(10);
+  doc.text(80, 500, info.pickupLine1);
+  doc.text(350, 500, info.returnLine1);
+
+  doc.text(80, 515, info.pickupLine2);
+  doc.text(350, 515, info.returnLine2);
+
+  doc.text(80, 530, info.pickupLine3);
+  doc.text(350, 530, info.returnLine3);
+
+  doc.line(50, 560, 560, 560);
+
+  doc.setFontSize(12);
+  doc.setFontType('bold');
+  doc.text(50, 580, 'Note:');
+  doc.setFontSize(11);
+  doc.setFontType('normal');
+  doc.text(50, 605, 'Terminal cut-off may change. The trucker has to check with the terminal prior');
+  doc.text(50, 625, 'to return.');
+
+  doc.text(50, 670, 'Best regards,');
+  doc.text(50, 710, 'OneStep Shipping.');
+
+  window.open(doc.output('bloburl'), '_blank');
+
+  return doc.output('blob');
+}
+
+export default pdfGenerator;
