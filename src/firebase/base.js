2fd92b1391710fbf9cd73af28186359fb6a6ba65
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-03 02:35:31 -0700

    Change Firebase Auth config

--- src/firebase/base.js
+++ src/firebase/base.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDj-llFQE-Dd-LDZ_qwWw5V-QLcIzmTYqw',
-  authDomain: 'shippose.firebaseapp.com',
-  databaseURL: 'https://shippose.firebaseio.com',
-  projectId: 'shippose',
-  storageBucket: 'shippose.appspot.com',
-  messagingSenderId: '156601874821',
-  appId: '1:156601874821:web:67fa4e711bac5cdf956cec',
-  measurementId: 'G-31QGR5BCLE',
+  apiKey: "AIzaSyB5VDSFbqtoa_N79cjfHQ5HbwA4na-Aje8",
+  authDomain: "onestep-shipping.firebaseapp.com",
+  databaseURL: "https://onestep-shipping.firebaseio.com",
+  projectId: "onestep-shipping",
+  storageBucket: "onestep-shipping.appspot.com",
+  messagingSenderId: "126413263035",
+  appId: "1:126413263035:web:9cae4e5a81108c7814d99e",
+  measurementId: "G-4JSJ2SKZF8"
 };
   // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
