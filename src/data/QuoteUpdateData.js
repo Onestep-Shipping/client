db7492ac1df32aa7ac24ec6f4864722840c373ec
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-06 14:19:04 -0700

    Add more data to Quote History

--- src/data/QuoteUpdateData.js
+++ src/data/QuoteUpdateData.js
@@ -1,5 +1,89 @@
 const QUOTE_DATA = [
   {
+    validityStart: "09/01/2020",
+    validityEnd: "09/30/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
+    validityStart: "08/01/2020",
+    validityEnd: "08/31/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
+    validityStart: "07/01/2020",
+    validityEnd: "07/31/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
+    validityStart: "06/01/2020",
+    validityEnd: "06/30/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
+    validityStart: "05/01/2020",
+    validityEnd: "05/31/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
+    validityStart: "04/01/2020",
+    validityEnd: "04/30/2020",
+    buying: {
+      oceanFreight: [750, 1000, 1300],
+      docFee: 60,
+      adminFee: 0
+    },
+    selling: {
+      oceanFreight: [800, 1200, 1600],
+      docFee: 80,
+      adminFee: 30
+    }
+  },
+  {
     validityStart: "03/01/2020",
     validityEnd: "03/31/2020",
     buying: {
@@ -15,7 +99,7 @@ const QUOTE_DATA = [
   },
   {
     validityStart: "02/01/2020",
-    validityEnd: "02/31/2020",
+    validityEnd: "02/28/2020",
     buying: {
       oceanFreight: [750, 1000, 1300],
       docFee: 60,
