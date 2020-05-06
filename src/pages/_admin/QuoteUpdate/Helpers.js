1687130922dad9939aa5ae04d9bc4d29206b08e2
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-06 16:20:59 -0700

    Fix small style

--- src/pages/_admin/QuoteUpdate/Helpers.js
+++ src/pages/_admin/QuoteUpdate/Helpers.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CONTAINER_TYPES } from '../../../constants/ServiceFormConstants';
-import styles from '../../../components/ScheduleForm/ScheduleFormMin.module.css';
 import { comma } from '../../../helpers/Helpers.js';
 
 const QuoteForm = props => {
@@ -11,16 +10,16 @@ const QuoteForm = props => {
       <h2>{header}</h2>
       {CONTAINER_TYPES.map((container, ind) =>
         <div className="invoice-row" key={ind}>
-          <text className={styles.scheduleLabel}>O.F. - {container}</text>
+          <text className="info-label">O.F. - {container}</text>
           <input type="number" name="oceanFreight" className="usd-input" required />
         </div>
       )}
       <div className="invoice-row">
-        <text className={styles.scheduleLabel}>Document Fee</text>
+        <text className="info-label">Document Fee</text>
         <input type="number" name="docFee" className="usd-input" required />
       </div>
       <div className="invoice-row">
-        <text className={styles.scheduleLabel}>Administration Fee</text>
+        <text className="info-label">Administration Fee</text>
         <input type="number" name="adminFee" className="usd-input" required />
       </div>
     </div>
@@ -33,7 +32,7 @@ const QuoteRow = props => {
   return (
     <div className="finance-display-form">
       <div className="finance-display-container">
-        <text className={styles.scheduleLabel}>{header}</text>
+        <text className="info-label">{header}</text>
       </div>
       {CONTAINER_TYPES.map((container, ind) =>
         <div className="finance-display-row" key={ind}>
