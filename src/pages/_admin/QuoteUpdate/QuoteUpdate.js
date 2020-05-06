ab9e3db73bed58752f2b6aa83537f2ad8a790146
scarletnguyen <scarlet.nguyen01@gmail.com>
2020-05-06 14:06:16 -0700

    Extract component to different helper file

--- src/pages/_admin/QuoteUpdate/QuoteUpdate.js
+++ src/pages/_admin/QuoteUpdate/QuoteUpdate.js
@@ -3,10 +3,10 @@ import './QuoteUpdate.css';
 
 import Header from '../../../components/Header/Header.js';
 import ScheduleForm from '../../../components/ScheduleForm/ScheduleForm.js';
-import { CONTAINER_TYPES } from '../../../constants/ServiceFormConstants';
 import QUOTE_DATA from '../../../data/QuoteUpdateData';
 import styles from '../../../components/ScheduleForm/ScheduleFormMin.module.css';
 import arrowDownIcon from '../../../assets/arrow-down.svg';
+import { QuoteForm, QuoteRow } from './Helpers';
 import moment from 'moment';
 
 const QuoteUpdate = () => {
@@ -63,52 +63,3 @@ const QuoteUpdate = () => {
 };
 
 export default QuoteUpdate;
-
-const QuoteForm = props => {
-  const { header } = props; 
-  return (
-    <div className="buying-container">
-      <h2>{header}</h2>
-      {CONTAINER_TYPES.map((container, ind) =>
-        <div className="invoice-row" key={ind}>
-          <text className={styles.scheduleLabel}>O.F. - {container}</text>
-          <input type="number" name="oceanFreight" className="usd-input" required />
-        </div>
-      )}
-      <div className="invoice-row">
-        <text className={styles.scheduleLabel}>Document Fee</text>
-        <input type="number" name="docFee" className="usd-input" required />
-      </div>
-      <div className="invoice-row">
-        <text className={styles.scheduleLabel}>Administration Fee</text>
-        <input type="number" name="adminFee" className="usd-input" required />
-      </div>
-    </div>
-  )
-}
-
-const QuoteRow = props => {
-  const { header, obj } = props;
-
-  return (
-    <div className="finance-display-form">
-      <div className="finance-display-container">
-        <text className={styles.scheduleLabel}>{header}</text>
-      </div>
-      {CONTAINER_TYPES.map((container, ind) =>
-        <div className="finance-display-row" key={ind}>
-          <text>O.F. - {container}:</text>
-          <text>{obj.oceanFreight[ind]}</text>
-        </div>
-      )}
-      <div className="finance-display-row">
-        <text>Doc fee:</text>
-        <text>{obj.docFee}</text>
-      </div>
-      <div className="finance-display-row">
-        <text>Admin fee:</text>
-        <text>{obj.adminFee}</text>
-      </div>
-    </div>
-  );
-}
